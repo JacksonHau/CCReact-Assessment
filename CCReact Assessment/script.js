f832e4cc5ba8ec8c77ad6368bb8532b173eb86c2
JacksonHau <Jacksonhaukeesean@gmail.com>
2025-04-11 22:06:33 +0200

    added some colour to the view1 webpage

---
+++ CCReact Assessment/script.js
@@ -0,0 +1,5 @@
+document.getElementById('startButton').addEventListener('click', function() {
+    console.log('Start Journey button clicked!');
+    window.location.href = 'view2.html'; // Redirect the user to the next view2
+  });
+  
